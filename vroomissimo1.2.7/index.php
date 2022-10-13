ca46b14890f0e85857856c49a6a316f821f8cd5a
Anthony <Degand.anthony@gmail.com>
2022-10-13 16:26:08 +0200

    13/10/22 16h26

---
+++ vroomissimo1.2.7/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include "/projet_vroomissimo/vroomissimo1.2.7/connection_bdd.php";
+
+include "/projet_vroomissimo/vroomissimo1.2.7/header.php";
+
+include "/projet_vroomissimo/vroomissimo1.2.7/footer.php";
+
+?>
