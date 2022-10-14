2933fb3589049b47f6934ba2aab031cb95eeb377
Anthony <Degand.anthony@gmail.com>
2022-10-14 11:43:10 +0200

    14/10/22 11:43

--- vroomissimo1.2.7/index.php
+++ vroomissimo1.2.7/index.php
@@ -1,9 +1,38 @@
 <?php
+// requier la pages pour déclarer mes fonctions
+require_once('C:\laragon\www\projet_vroomissimo\vroomissimo1.2.7\includes.php\functions.php');
+//inclure mon header
+ include('C:\laragon\www\projet_vroomissimo\vroomissimo1.2.7\includes.php\header.php');
+//inclure ma navbar
+include('C:\laragon\www\projet_vroomissimo\vroomissimo1.2.7\includes.php\navbar.php');
 
-include "/projet_vroomissimo/vroomissimo1.2.7/connection_bdd.php";
+// requier la connection à ma base de donnée 
+require_once('C:\laragon\www\projet_vroomissimo\vroomissimo1.2.7\includes.php\connection_bdd.php');
 
-include "/projet_vroomissimo/vroomissimo1.2.7/header.php";
+// on écrit la requête
+$sql = "SELECT * FROM `vehicules` ORDER BY `id_vehicules` DESC";
+// on exécute la requête
+$requete = $db->query($sql); // dans ma bbd exécute ma requete "sql"
+// on récupère les données
+$vehicules = $requete->fetchAll(); // variable vehicules = récupèrer les données de la variable "requete" (info : apres un fetch all il y a une boucle )
 
-include "/projet_vroomissimo/vroomissimo1.2.7/footer.php";
+// contenue de ma pages
+?>
+<h1>Contenue de la page d'accueil</h1>
+
+<section> <!--ouvrir une section pour le SEO (referencement) est une bonne pratique-->
+<?php foreach($vehicules as $vehicule): ?> <!-- boucle "pour chaque" qui parcourt les tableaux, ici dans la table vehicules pour chaques vehicule : -->
+    <article>
+        <h1><?= strip_tags($vehicule['id_vehicules'])?></h1>        <!-- afficher les id_vehicules avec la methode "strip_tags()" pour prévenir les injections sql-->
+        <p><?= strip_tags($vehicule['prix_vente'])?></p>            <!-- afficher les prix_vente-->
+        <p>Statut : <?=strip_tags($vehicule['statut_voiture'])?></p><!-- afficher statut_voiture-->   
+    </article>
+<?php endforeach; ?>
+</section>
+
+<?php
+
+// inclure mon footer
+include('C:\laragon\www\projet_vroomissimo\vroomissimo1.2.7\includes.php\footer.php');
 
 ?>
